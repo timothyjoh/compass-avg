11b9f7b66b3bb70b99e1631a24d145c6df54133a
Tim Johnson <tjohnson@scitent.com>
2018-10-28 20:47:01 -0400

    silly change, but could handle compasses defined in different ranges, besides 0-360

--- compass.js
+++ compass.js
@@ -1,12 +1,13 @@
+const RANGE = 360
+const HALFRANGE = RANGE / 2
 export const average = (h1,h2) => (h1 + h2) / 2
 export const max = (h1, h2) => h1 >= h2 ? h1 : h2
 export const min = (h1, h2) => h1 < h2 ? h1 : h2
-export const difference = (h1, h2) => (
-  max(h1,h2) - min(h1,h2)
-)
+export const difference = (h1, h2) => max(h1,h2) - min(h1,h2)
+const reverseHeading = (h1, h2) => difference(h1,h2) > HALFRANGE
 
 export const compassAverage = (h1, h2) => (
   (average(h1,h2) +
-    (difference(h1, h2) > 180 ? 180 : 0)
-  ) % 360
+    (reverseHeading(h1, h2) ? HALFRANGE : 0)
+  ) % RANGE
 );
